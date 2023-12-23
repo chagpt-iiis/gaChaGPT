3cafd12817fa4f3a62d158490e756111fbfbe452
yhx-12243 <yhx12243@gmail.com>
2023-12-23 17:59:38 +0800

    feat: consistency test passed, 该版本仅为测试，请勿使用！！！

--- src/models/chagpt-gacha-new.js
+++ src/models/chagpt-gacha-new.js
@@ -1,3 +1,5 @@
+import mt19937 from '@stdlib/random-base-mt19937';
+
 export default class ChagptGachaNew {
     constructor() {
         this.drops = this.generateDrops();
@@ -18,27 +20,72 @@ export default class ChagptGachaNew {
         this.pityCounter5 = 0;
     }
 
-    roll(amount=5) {
-        // console.log('amount', amount)
+    roll(amount = 5) {
+        let seed = new Uint32Array(unitTestSeeds.pop());
+
+        let i;
+        console.log('seed =', seed);
+        seed.reverse();
+        for (i = seed.length; i > 0 && !seed[i - 1]; --i);
+        const rng = mt19937.factory({ seed: seed.subarray(0, i) });
+
         const roll = [];
         for (let i = 0; i < amount; i++) {
-            roll.push(this.rollOnce());
+            roll.push(this.rollOnceWithRng(rng));
         }
 
-        // console.log('this.attemptsCount', this.attemptsCount)
-        // console.log('this.threshold3', this.threshold3)
-        // console.log('this.threshold4', this.threshold4)
-        // console.log('this.threshold5', this.threshold5)
-
         return roll;
+    }
+
+    rollOnceWithRng(rng) {
+        console.log(this.drops.length, this.prizePool.length);
+
+        this.attemptsCount++;
+
+        const idx = this.randBelowWithRng(this.drops.length, rng);
+        const item = this.drops.splice(idx, 1)[0];
+
+        if (this.attemptsCount === (this.threshold3 + this.pity3Limit)) {
+            const index = this.prizePool.findIndex(prize => prize.level === '3-star');
+            if (index < 0) {
+                console.error('not enough 3-star!!!');
+            } else {
+                this.threshold3 += this.pity3Limit;
+                this.assignPrizeToItem(index, item);
+            }
+            return item;
+        }
+
+        const pool = this.prizePool.filter(prize => {
+            switch (prize.level) {
+                case '5-star':
+                    return this.attemptsCount > this.threshold5;
+                case '4-star':
+                    return this.attemptsCount > this.threshold4;
+                case '3-star':
+                    return this.attemptsCount > this.threshold3;
+                default:
+                    return true;
+            }
+        });
+
+        const index = this.randBelowWithRng(pool.length, rng);
+        const prize = pool[index];
+        if (prize.level === '3-star') {
+            this.threshold3 += this.pity3Limit;
+        }
+        this.assignPrizeToItem(index, item);
+
+        return item;
     }
 
     rollOnce() {
         this.attemptsCount++;
 
         this.shuffleArray(this.drops);
-        this.shuffleArray(this.prizePool);
         const item = this.drops.pop();
+
+        this.shuffleArray(this.prizePool);
 
         for (let idx = 0; idx < this.prizePool.length; idx++) {
             const prize = this.prizePool[idx];
@@ -94,7 +141,7 @@ export default class ChagptGachaNew {
         this.assignedPrize.push(prize);
     }
 
-    generateDrops(lastIdxDict = { 'tea_a': 199, 'tea_b': 199, 'tea_c': 199, 'tea_d': 199, 'tea_e': 199, 'tea_f': 199 }) {
+    generateDrops(lastIdxDict = { 'tea_a': 200, 'tea_b': 200, 'tea_c': 200, 'tea_d': 200, 'tea_e': 200, 'tea_f': 200 }) {
         const drops = [];
         let uniqueId = 0;
 
@@ -104,12 +151,13 @@ export default class ChagptGachaNew {
                 const src = `${teaType}.png`;
                 const teaName = this.TeaType2TeaName(teaType);
                 const teaNameId = `${teaName}-${j}`;
+                const debugTeaName = teaType[4].toUpperCase();
 
                 drops.push({
                     teaType: teaType,
                     id: j,
                     src: src,
-                    teaName: teaName,
+                    teaName: debugTeaName,
                     teaNameId: teaNameId,
                     uniqueId: uniqueId,
                     assigned: false,
@@ -132,7 +180,7 @@ export default class ChagptGachaNew {
             case 'tea_b':
                 return '凤凰单枞'
             case 'tea_c':
-                return '玫瑰红茶'
+                return '玫瑰花茶'
             case 'tea_d':
                 return '锡兰红茶'
             case 'tea_e':
@@ -233,4 +281,33 @@ export default class ChagptGachaNew {
             [array[i], array[j]] = [array[j], array[i]];
         }
     }
+
+    randBelowWithRng(n, rng) {
+        if (!n) {
+            console.error('call rand(0) !!!');
+            return 0;
+        }
+        const k = Math.clz32(n);
+        let r;
+        do { r = rng() >>> k; } while (r >= n);
+        return r;
+    }
 }
+
+const unitTestSeeds = [
+    [0xdbeb6048, 0xf8139dfc, 0x2253f73f, 0x4b3d5e87, 0x493a3fec, 0x0af39fd2, 0xcbbc1c8f, 0x4b496fd0],
+    [0x1cac36ed, 0x4378f6a3, 0x21beae4f, 0xce83ed26, 0x224b3d28, 0x3585558b, 0xff0b5498, 0x2802c792],
+    [0x11a082a8, 0x125e0ff0, 0x3384e72f, 0x4dd7e6b4, 0x1f4f32f0, 0xf6eccdda, 0xca82346d, 0x4df4a478],
+    [0x16e680f5, 0xd5e53d3d, 0x41967b43, 0x51fdd84a, 0x6e625b9b, 0x5b2dcf76, 0xf4a4e77f, 0xf9edeb5c],
+    [0x17b26a2e, 0xb8135230, 0x76d74133, 0xba59995d, 0xb7fe7047, 0x2d16f931, 0x677ff724, 0xc6313e65],
+    [0x4ee61bc3, 0x11d05a62, 0x59574bc3, 0xf3991473, 0x522796a3, 0xe6632082, 0xc02707c4, 0xad9ee50c],
+    [0x8edccd9b, 0xcec832ab, 0xfe6bf988, 0xa3ee8fdc, 0x290b0fdc, 0xf18e259b, 0x16c606e3, 0x86a46c29],
+    [0x4957ba25, 0xcbc03a13, 0xa8e7ed10, 0xc875b996, 0xd08f4334, 0x47df44c8, 0xab5ca484, 0xaad2ff47],
+    [0x523e3cb7, 0xded51c27, 0x66604d82, 0x61d109f3, 0xd5856c1b, 0x723f18d5, 0x79a3063b, 0x99fa9090],
+    [0x534b4f85, 0x6500bedd, 0xac895d71, 0xd15fa124, 0xcc3e6d0d, 0x61b38bdb, 0xaea5016d, 0x67cddd51],
+    [0x32146b90, 0x724207f0, 0x2095dd18, 0xee4e01b3, 0x1d19627d, 0xe6644839, 0x05c6b3ad, 0x7b0ec481],
+    [0xc70f7882, 0x3b980361, 0x1aa6e91c, 0x39d60371, 0xef1b8f76, 0xbb2ba286, 0x7b02f498, 0x6c1b733d],
+    [0x73431008, 0x25c416e8, 0x0a419aa1, 0x326bb744, 0x52bd698b, 0x2b1af8c0, 0xbbcf62a8, 0x904b3e7d],
+    [0xc1ff735e, 0x8e1def6f, 0xc25328a4, 0x180621b6, 0xfb04f465, 0x26bb90fa, 0x1722e40c, 0x0e83f003],
+    [0x6c07847e, 0xf2f9fd47, 0xec4bf36b, 0xed88d93e, 0x179870b4, 0x91c16b02, 0xe302573b, 0xb4d8a094],
+];
